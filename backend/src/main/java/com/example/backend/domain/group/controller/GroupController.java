356af89f9349fcd30292a035bd71cd375ff44119
kjm722 <kjm722@naver.com>
2025-01-27 11:51:55 +0900

    Feat : 그룹 생성 기능 추가
    - Group 엔티티 ownerId로 변경

--- backend/src/main/java/com/example/backend/domain/group/controller/GroupController.java
+++ backend/src/main/java/com/example/backend/domain/group/controller/GroupController.java
@@ -1,10 +1,13 @@
 package com.example.backend.domain.group.controller;
 
+import com.example.backend.domain.group.dto.GroupRequestDto;
+import com.example.backend.domain.group.dto.GroupResponseDto;
 import com.example.backend.domain.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -13,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
     private final GroupService groupService;
 
+    @PostMapping("/{ownerId}")
+    public ResponseEntity<GroupResponseDto>createGroup(@PathVariable Long ownerId, @RequestBody GroupRequestDto requestDto) {
+        GroupResponseDto response = groupService.create(ownerId, requestDto);
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+    }
 }
--- backend/src/main/java/com/example/backend/domain/group/service/GroupService.java
+++ backend/src/main/java/com/example/backend/domain/group/service/GroupService.java
@@ -1,10 +1,31 @@
 package com.example.backend.domain.group.service;
 
+import com.example.backend.domain.group.dto.GroupRequestDto;
+import com.example.backend.domain.group.dto.GroupResponseDto;
+import com.example.backend.domain.group.entity.Group;
+import com.example.backend.domain.group.entity.GroupStatus;
+import com.example.backend.domain.group.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class GroupService {
+    private final GroupRepository groupRepository;
+
+    @Transactional
+    public GroupResponseDto create(Long ownerId, GroupRequestDto groupRequestDto){
+        Group group = Group.builder()
+                .title(groupRequestDto.getTitle())
+                .description(groupRequestDto.getDescription())
+                .status(GroupStatus.ACTIVE)
+                .ownerId(ownerId)
+                .maxParticipants(groupRequestDto.getMaxParticipants())
+                .build();
+        groupRepository.save(group);
+        return new GroupResponseDto(group);
+    }
+
 }
 
