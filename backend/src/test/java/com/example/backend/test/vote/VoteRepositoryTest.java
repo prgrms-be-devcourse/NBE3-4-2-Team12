7352f9e33a0a7adc831e0e6e62c6875c1f023ece
notmybussiness <cuisine97@naver.com>
2025-01-27 11:42:24 +0900

    test: VoteRepository 데이터 저장 test

--- backend/src/test/java/com/example/backend/test/vote/VoteRepositoryTest.java
+++ backend/src/test/java/com/example/backend/test/vote/VoteRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @DataJpaTest
 public class VoteRepositoryTest {
 
@@ -21,12 +23,15 @@ public class VoteRepositoryTest {
         vote.setGroupId(1L);
         vote.setLocation("강남역");
         vote.setAddress("서초구 서초동");
-        vote.setLatitude("33.333");
-        vote.setLongitude("127.333");
+        vote.setLatitude(33.333);
+        vote.setLongitude(127.333);
         vote.setVoterCount(0);
         //when
         Vote savedVote = voteRepository.save(vote);
         //then
-
+        assertThat(savedVote.getId()).isNotNull();
+        assertThat(savedVote.getGroupId()).isEqualTo(1L);
+        assertThat(savedVote.getLocation()).isEqualTo("강남역");
+        assertThat(savedVote.getAddress()).isEqualTo("서초구 서초동");
     }
 }
