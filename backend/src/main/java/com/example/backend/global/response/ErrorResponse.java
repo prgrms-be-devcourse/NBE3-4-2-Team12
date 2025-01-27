3e592e09c9866dc7102665b98ff5907ac54b973f
100gammmit <100minha@naver.com>
2025-01-27 15:51:43 +0900

    feat: 예외 발생 시 사용할 공통 응답 객체 작성 (#11)

--- backend/src/main/java/com/example/backend/global/response/ErrorResponse.java
+++ backend/src/main/java/com/example/backend/global/response/ErrorResponse.java
@@ -24,16 +24,16 @@ public record ErrorResponse(
 	 */
 	@Builder
 	public static record ValidationError(
-		String field,    // 에러가 발생한 필드명
-		String message    // 해당 필드의 에러 메시지
+		String field,	// 에러가 발생한 필드명
+		String message	// 해당 필드의 에러 메시지
 	) {
 	}
 
 	/**
 	 * Validation 예외 발생 시 응답 때 사용할 팩토리 메서드
-	 * @param message    // 예외 메세지
-	 * @param status    // 예외 상태 코드
-	 * @param errors    // 필드별 에러 메세지
+	 * @param message	// 예외 메세지
+	 * @param status	// 예외 상태 코드
+	 * @param errors	// 필드별 에러 메세지
 	 */
 	public static ErrorResponse of(String message, String status, List<ValidationError> errors) {
 		return ErrorResponse.builder()
@@ -45,8 +45,8 @@ public record ErrorResponse(
 
 	/**
 	 * 이외에 FieldError를 포함하지 않는 예외 발생 시 사용할 팩토리 메서드
-	 * @param message    // 예외 메세지
-	 * @param status    // 예외 상태 코드
+	 * @param message	// 예외 메세지
+	 * @param status	// 예외 상태 코드
 	 */
 	public static ErrorResponse of(String message, String status) {
 		return ErrorResponse.builder()
