ad0e81fe2702ac21de548df062503e328f74b979
kjm722 <kjm722@naver.com>
2025-01-27 17:08:15 +0900

    Fix : GroupResponseDto status 추가 및 gitignore 수정

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.backend;
 
 import com.example.backend.domain.group.dto.GroupRequestDto;
 import com.example.backend.domain.group.service.GroupService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +14,15 @@ class BackendApplicationTests {
 	private GroupService groupService;
 
 	@Test
+	@DisplayName("그룹 생성")
 	void contextLoads() {
 		groupService.create(1L,new GroupRequestDto("제목1","내용1",5));
+	}
+
+	@Test
+	@DisplayName("모임 전체 조회")
+	void t2() {
+		groupService.findAllGroups();
 	}
 
 }
