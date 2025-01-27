caace39876c881dd59cebd13e0b92c6624c51c72
notmybussiness <cuisine97@naver.com>
2025-01-27 15:22:50 +0900

    fix: Voter - Vote - User table간의 관계 고민

---
+++ backend/src/main/java/com/example/backend/domain/vote/dto/VoteDTO.java
@@ -0,0 +1,35 @@
+package com.example.backend.domain.vote.dto;
+
+import com.example.backend.domain.vote.entity.Vote;
+import lombok.Getter;
+import org.hibernate.bytecode.internal.bytebuddy.BytecodeProviderImpl;
+
+@Getter
+public class VoteDTO {
+    private String location;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
+    // Entity -> DTO
+    public static VoteDTO from(Vote vote) {
+        VoteDTO voteDto = new VoteDTO();
+        voteDto.location = vote.getLocation();
+        voteDto.address = vote.getAddress();
+        voteDto.latitude = vote.getLatitude();
+        voteDto.longitude = vote.getLongitude();
+        return voteDto;
+    }
+
+    // DTO -> Entity
+    public Vote toEntity(Long groupId) {
+        return Vote.builder()
+                .groupId(groupId)
+                .location(this.location)
+                .address(this.address)
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .voterCount(0)
+                .build();
+    }
+}
