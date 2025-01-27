8264b9f1bd35a7605ec19380826cc20f82a74ad5
notmybussiness <cuisine97@naver.com>
2025-01-27 15:22:50 +0900

    fix: Voter - Vote - User table간의 관계 고민

--- backend/src/main/java/com/example/backend/domain/vote/controller/VoteController.java
+++ backend/src/main/java/com/example/backend/domain/vote/controller/VoteController.java
@@ -1,4 +1,19 @@
 package com.example.backend.domain.vote.controller;
 
+import com.example.backend.domain.vote.dto.VoteDTO;
+import com.example.backend.domain.vote.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/votes")
 public class VoteController {
+    private final VoteService voteService;
+
+    @PostMapping("/{groupId}")
+    public VoteDTO createVote(@PathVariable Long groupId,
+                              @RequestBody VoteDTO voteDTO){
+        return VoteDTO.from(voteService.createVote(groupId, voteDTO.toEntity(groupId)));
+    }
 }
