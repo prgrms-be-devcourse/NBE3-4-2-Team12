ff00eb94843cfe6062364b855d48885c2e5d7f43
wlsdnjs1120 <wlsdnjs1120@naver.com>
2025-01-31 15:46:47 +0900

    feat: 투표에 참여한 참여자 목록 조회

--- backend/src/main/java/com/example/backend/domain/voter/service/VoterService.java
+++ backend/src/main/java/com/example/backend/domain/voter/service/VoterService.java
@@ -2,10 +2,23 @@ package com.example.backend.domain.voter.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.example.backend.domain.voter.dto.VoterDTO;
+import com.example.backend.domain.voter.entity.Voter;
+import com.example.backend.domain.voter.repository.VoterRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class VoterService {
+    private final VoterRepository voterRepository;
 
+    // 특정 투표에 참여한 Voter 목록 조회
+    public List<VoterDTO> getVotersByVoteId(Long voteId) {
+        List<Voter> voters = voterRepository.findByIdVoteId(voteId);
+        return voters.stream()
+                .map(VoterDTO::from) // Voter 엔티티를 VoterDTO로 변환
+                .collect(Collectors.toList());
+    }
 }
 
