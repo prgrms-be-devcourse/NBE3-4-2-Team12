0b15f96880ef0de136f1e2e38233bd7c9152e2b2
100gammmit <100minha@naver.com>
2025-02-03 15:45:37 +0900

    feat: 카카오에서 관리하는 사용자uid를 사용하여 기존 사용자인지 검증하는 메서드 추가 (#34)

--- backend/src/main/java/com/example/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/example/backend/domain/member/repository/MemberRepository.java
@@ -1,9 +1,12 @@
 package com.example.backend.domain.member.repository;
 
-import com.example.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.backend.domain.member.entity.Member;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	boolean existsByKakaoId(Long kakaoId);
 }
