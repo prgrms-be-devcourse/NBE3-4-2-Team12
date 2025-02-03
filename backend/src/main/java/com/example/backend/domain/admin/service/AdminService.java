231aad3dd67a1ee7cc431f87cd4ea3f074c09dde
gorhkd <gorhkd223@gmail.com>
2025-02-03 22:53:30 +0900

    Refactor : Admin 리파지토리, 서비스 예외처리

--- backend/src/main/java/com/example/backend/domain/admin/service/AdminService.java
+++ backend/src/main/java/com/example/backend/domain/admin/service/AdminService.java
@@ -1,13 +1,14 @@
 package com.example.backend.domain.admin.service;
 
 import com.example.backend.domain.admin.entity.Admin;
+import com.example.backend.domain.admin.exception.AdminErrorCode;
+import com.example.backend.domain.admin.exception.AdminException;
 import com.example.backend.domain.admin.repository.AdminRepository;
-import com.example.backend.global.config.JwtProvider;
+import com.example.backend.global.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @RequiredArgsConstructor
 @Service
@@ -20,14 +21,14 @@ public class AdminService {
     public String login(String adminName, String password) {
         // 아이디로 관리자 조회
         Admin admin = adminRepository.findByAdminName(adminName)
-                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 관리자입니다."));
+                .orElseThrow(() -> new AdminException(AdminErrorCode.NOT_FOUND_ADMIN));
 
         // 비밀번호 검증
         if(!passwordEncoder.matches(password, admin.getPassword())) {
-            throw new BadCredentialsException("비밀번호가 틀렸습니다.");
+            throw new AdminException(AdminErrorCode.INVALID_CREDENTIALS);
         }
 
         // JWT  토큰 발급
-        return jwtProvider.generateToken(admin.getAdminName(), admin.getRole().name());
+        return jwtProvider.generateToken(admin);
     }
 }
