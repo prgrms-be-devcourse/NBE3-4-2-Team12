47bba094fe5aa307f6d46f198fb8a196d938c506
kjm722 <kjm722@naver.com>
2025-02-04 11:41:04 +0900

    Feat : 회원이 속한 그룹 조회

--- backend/src/main/java/com/example/backend/domain/groupmember/repository/GroupMemberRepository.java
+++ backend/src/main/java/com/example/backend/domain/groupmember/repository/GroupMemberRepository.java
@@ -5,7 +5,10 @@ import com.example.backend.domain.groupmember.entity.GroupMember;
 import com.example.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
     boolean existsByGroupAndMember(Group group, Member member);
     long countByGroup(Group group);
+    List<GroupMember> findByMember(Member member);
 }
