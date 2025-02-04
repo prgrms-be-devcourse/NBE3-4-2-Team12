7f320e3b52da8bd9010a3325d151e57d7823d1ad
100gammmit <100minha@naver.com>
2025-02-04 14:05:47 +0900

    feat: 쿠키 관련 로직 수행 서비스 클래스 작성 (#56)

--- backend/src/main/java/com/example/backend/global/auth/service/CookieService.java
+++ backend/src/main/java/com/example/backend/global/auth/service/CookieService.java
@@ -3,7 +3,10 @@ package com.example.backend.global.auth.service;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.global.auth.util.CookieUtil;
+import com.example.backend.global.auth.util.JwtUtil;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -16,5 +19,28 @@ import lombok.RequiredArgsConstructor;
 public class CookieService {
 
 	private final CookieUtil cookieUtil;
-	
+	private final JwtUtil jwtUtil;
+
+	public void addAccessTokenToCookie(String accessToken, HttpServletResponse response) {
+		cookieUtil.addTokenToCookie("accessToken", accessToken,
+			jwtUtil.getAccessTokenExpirationTime(), response);
+	}
+
+	public void addRefreshTokenToCookie(String refreshToken, HttpServletResponse response) {
+		cookieUtil.addTokenToCookie("refreshToken", refreshToken,
+			jwtUtil.getRefreshTokenExpirationTime(), response);
+	}
+
+	public String getAccessTokenFromCookie(HttpServletRequest request) {
+		return cookieUtil.getTokenFromCookie("accessToken", request);
+	}
+
+	public String getRefreshTokenFromCookie(HttpServletRequest request) {
+		return cookieUtil.getTokenFromCookie("refreshToken", request);
+	}
+
+	public void clearTokenFromCookie(HttpServletResponse response) {
+		cookieUtil.addTokenToCookie("accessToken", null, 0, response);
+		cookieUtil.addTokenToCookie("refreshToken", null, 0, response);
+	}
 }
