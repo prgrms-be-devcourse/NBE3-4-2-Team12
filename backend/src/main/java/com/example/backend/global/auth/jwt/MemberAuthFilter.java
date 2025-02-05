74a60e6da6481ab9127e8010877e4b8c77de40e7
100gammmit <100minha@naver.com>
2025-02-05 11:47:00 +0900

    feat: Member전용 인증 필터 작성중

--- backend/src/main/java/com/example/backend/global/auth/jwt/MemberAuthFilter.java
+++ backend/src/main/java/com/example/backend/global/auth/jwt/MemberAuthFilter.java
@@ -1,11 +1,18 @@
 package com.example.backend.global.auth.jwt;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.example.backend.global.auth.service.CookieService;
+import com.example.backend.global.auth.util.JwtUtil;
+
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 /**
  * MemberAuthFilter
@@ -13,11 +20,26 @@ import jakarta.servlet.http.HttpServletResponse;
  * @author 100minha
  */
 @Configuration
+@RequiredArgsConstructor
 public class MemberAuthFilter extends OncePerRequestFilter {
+
+	private final CookieService cookieService;
+	private final JwtUtil jwtUtil;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) {
+		FilterChain filterChain) throws ServletException, IOException {
+
+		String accessToken = cookieService.getAccessTokenFromCookie(request);
+		if (accessToken == null) {
+			String refreshToken = cookieService.getRefreshTokenFromCookie(request);
+			if (refreshToken == null) {
+				return;
+			}
+
+			filterChain.doFilter(request, response);
+			return;
+		}
 
 	}
 }
