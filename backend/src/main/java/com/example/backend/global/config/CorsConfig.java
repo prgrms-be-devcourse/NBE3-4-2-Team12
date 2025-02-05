50565a85bf1bc9e242aa033b06c8b28f7689722a
100gammmit <100minha@naver.com>
2025-02-05 09:29:32 +0900

    feat: Cors설정 클래스 작성  (#73)

---
+++ backend/src/main/java/com/example/backend/global/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.example.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * CorsConfig
+ * Cors설정 클래스
+ * @author 100minha
+ */
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowCredentials(true);
+		corsConfig.addAllowedOrigin("https://localhost:3000");
+		corsConfig.addAllowedHeader("*");
+		corsConfig.addExposedHeader("Authorization");
+		corsConfig.addAllowedMethod("*");
+
+		source.registerCorsConfiguration("/**", corsConfig);
+		return new CorsFilter(source);
+	}
+}
