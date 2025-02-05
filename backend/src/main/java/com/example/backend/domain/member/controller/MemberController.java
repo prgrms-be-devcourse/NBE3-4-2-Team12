4e4a2fcb74c43378d545291b244dfbedb29380fc
100gammmit <100minha@naver.com>
2025-02-05 17:37:18 +0900

    Feat: 사용자 정보 단건 조회 구현 및 조회 시 사용할 dto클래스 작성 (#73)

--- backend/src/main/java/com/example/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/domain/member/controller/MemberController.java
@@ -1,18 +1,33 @@
 package com.example.backend.domain.member.controller;
 
-import com.example.backend.domain.member.service.MemberService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.domain.member.dto.MemberResponseDto;
+import com.example.backend.domain.member.service.MemberService;
+import com.example.backend.global.auth.model.CustomUserDetails;
+import com.example.backend.global.response.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/members")
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
 
-    private final MemberService memberService;
+	private final MemberService memberService;
 
+	@GetMapping
+	public ResponseEntity<ApiResponse<MemberResponseDto>> findMember(
+		@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+		Long id = customUserDetails.getUserId();
+		System.out.println("customUserDetails.getUsername() = " + customUserDetails.getUsername());
+		System.out.println("customUserDetails = " + customUserDetails.getEmail());
 
-
-
+		MemberResponseDto memberDto = memberService.findMemberResponseDtoById(id);
+		return ResponseEntity.ok().body(ApiResponse.of(memberDto));
+	}
 }
