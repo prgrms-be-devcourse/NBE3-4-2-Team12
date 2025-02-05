7efbf6f0e6f2de81ebbde03b0027b6f31b02495d
100gammmit <100minha@naver.com>
2025-02-05 15:38:34 +0900

    Refactor: 회원 가입과 카카오 토큰 갱신 로직을 분리하도록 수정 (#73)

--- backend/src/main/java/com/example/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/example/backend/domain/member/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.example.backend.domain.member.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.backend.domain.member.entity.Member;
+import com.example.backend.domain.member.exception.MemberErrorCode;
+import com.example.backend.domain.member.exception.MemberException;
 import com.example.backend.domain.member.repository.MemberRepository;
 import com.example.backend.global.auth.dto.KakaoUserInfoResponseDto;
 
@@ -14,17 +17,42 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 
+	@Transactional(readOnly = true)
+	public Member findById(Long id) {
+
+		return memberRepository.findById(id).orElseThrow(() ->
+			new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+	}
+
+	@Transactional(readOnly = true)
+	public Member findByKakaoId(Long kakaoId) {
+
+		return memberRepository.findByKakaoId(kakaoId).orElseThrow(() ->
+			new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+	}
+
+	@Transactional(readOnly = true)
 	public boolean existsByKakaoId(Long kakaoId) {
 
 		return memberRepository.existsByKakaoId(kakaoId);
 	}
 
-	public void join(KakaoUserInfoResponseDto kakaoUserInfoDto,
-		String kakaoAccessToken, String kakaoRefreshToken) {
+	@Transactional
+	public void join(KakaoUserInfoResponseDto kakaoUserInfoDto) {
 
 		Member member = Member.of(kakaoUserInfoDto);
-		member.updateAccessToken(kakaoAccessToken);
-		member.updateRefreshToken(kakaoRefreshToken);
-		memberRepository.save(member);
+		Member savedMember = memberRepository.save(member);
+	}
+
+	@Transactional
+	public void updateAccessToken(Member member, String accessToken) {
+
+		member.updateAccessToken(accessToken);
+	}
+
+	@Transactional
+	public void updateRefreshToken(Member member, String refreshToken) {
+
+		member.updateRefreshToken(refreshToken);
 	}
 }
