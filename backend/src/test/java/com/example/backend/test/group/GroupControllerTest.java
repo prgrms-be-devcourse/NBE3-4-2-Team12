57abf006e9ca147e71c9fcd99edd35754279c968
kjm722 <kjm722@naver.com>
2025-02-05 14:32:56 +0900

    Feat : 그룹 생성 테스트

--- backend/src/test/java/com/example/backend/test/group/GroupControllerTest.java
+++ backend/src/test/java/com/example/backend/test/group/GroupControllerTest.java
@@ -1,22 +1,68 @@
 package com.example.backend.test.group;
 
+import com.example.backend.domain.group.controller.GroupController;
 import com.example.backend.domain.group.dto.GroupRequestDto;
+import com.example.backend.domain.group.dto.GroupResponseDto;
+import com.example.backend.domain.group.entity.GroupStatus;
 import com.example.backend.domain.group.service.GroupService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class GroupControllerTest {
 
     @Autowired
     private GroupService groupService;
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
     @DisplayName("그룹 생성")
-    void contextLoads() {
-        groupService.create(new GroupRequestDto("제목1","내용1",5,1L));
+    void t1() throws Exception {
+        ResultActions resultActions = mvc.perform(
+                post("/groups")
+                        .content("""
+                                {
+                                  "title": "제목1",
+                                  "description": "내용1",
+                                  "memberId":1,
+                                  "maxParticipants":5,
+                                  "categoryIds": [1],
+                                  "status":"RECRUITING"
+                                }
+                                """)
+                        .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+        ).andDo(print());
+
+        GroupResponseDto groupResponseDto = groupService.create(new GroupRequestDto("제목1","내용1",5,1L, Arrays.asList(1L), GroupStatus.RECRUITING));
+        System.out.println("Generated Group ID: " + groupResponseDto.getId());
+        resultActions.andExpect(handler().handlerType(GroupController.class))
+                .andExpect(handler().methodName("createGroup"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").value(groupResponseDto.getTitle()))
+                .andExpect(jsonPath("$.description").value(groupResponseDto.getDescription()))
+                .andExpect(jsonPath("$.memberId").value(groupResponseDto.getMemberId()))
+                .andExpect(jsonPath("$.maxParticipants").value(groupResponseDto.getMaxParticipants()))
+                .andExpect(jsonPath("$.category").isArray())
+                .andExpect(jsonPath("$.category[0].id").value(1L))
+                .andExpect(jsonPath("$.status").value(Matchers.is("RECRUITING")));
     }
 
     @Test
