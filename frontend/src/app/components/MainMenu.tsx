2aa665825e053a634715f027ec29f0b320af661c
kjm722 <kjm722@naver.com>
2025-02-06 17:23:29 +0900

    Feat : 프론트엔트 카카오 로그인 구현

--- frontend/src/app/components/MainMenu.tsx
+++ frontend/src/app/components/MainMenu.tsx
@@ -1,14 +1,29 @@
-import Link from 'next/link';
+"use client";
+
+import Link from "next/link";
+import { kakaoLogin } from "../api/auth";
+
+const handleKakaoLogin = async () => {
+  kakaoLogin();
+};
 
 const MainMenu = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-100 shadow-md">
       <h1 className="text-lg font-bold">MOYODANG</h1>
       <div className="space-x-4">
-        <button className="bg-green-500 text-white px-4 py-2 rounded">모임 만들기</button>
-        <Link href="/profile" className="text-gray-700">내정보</Link>
-        <Link href="/signup" className="text-gray-700">회원가입</Link>
-        <Link href="/login" className="text-gray-700">로그인</Link>
+        <button className="bg-green-500 text-white px-4 py-2 rounded">
+          모임 만들기
+        </button>
+        <Link href="/profile" className="text-gray-700">
+          내정보
+        </Link>
+        <Link href="/signup" className="text-gray-700">
+          회원가입
+        </Link>
+        <button onClick={handleKakaoLogin} className="text-gray-700">
+          로그인
+        </button>
       </div>
     </nav>
   );
