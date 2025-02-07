907d1d451f7a3550938cc1fa5785e0afb30f0f75
gorhkd <gorhkd223@gmail.com>
2025-02-07 09:21:16 +0900

    Feat : 경로별 admin 필터와 member 필터 구분

--- backend/src/main/java/com/example/backend/global/auth/jwt/AdminAuthFilter.java
+++ backend/src/main/java/com/example/backend/global/auth/jwt/AdminAuthFilter.java
@@ -3,6 +3,7 @@ package com.example.backend.global.auth.jwt;
 
 import com.example.backend.domain.admin.entity.Admin;
 import com.example.backend.domain.admin.repository.AdminRepository;
+import com.example.backend.global.auth.service.CookieService;
 import com.example.backend.global.auth.util.JwtUtil;
 import com.example.backend.global.auth.util.TokenProvider;
 import jakarta.servlet.FilterChain;
@@ -30,44 +31,54 @@ public class AdminAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
     throws ServletException, IOException {
 
-        String path = request.getServletPath();
-
-        // 로그인 요청은 필터 통과
-        if (path.equals("/admin/login")) {
-            chain.doFilter(request, response);
-            return;
-        }
-
         // 요청 헤더에서 JWT 토큰 가져오기
         String accessToken = this.cookieService.getAccessTokenFromCookie(request);
+        String refreshToken = this.cookieService.getRefreshTokenFromCookie(request);
 
         // 토큰이 유효하면 SecurityContext 에 인증 정보 저장
         if (accessToken == null) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-        if (jwtUtil.validateToken(accessToken)) {
-            Authentication authentication = jwtUtil.getAuthentication(accessToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            chain.doFilter(request, response);
-            return;
-        } else if (jwtUtil.isTokenExpired(accessToken)) {
-            String refreshToken = this.cookieService.getRefreshTokenFromCookie(request);
-
-            if (refreshToken != null && jwtUtil.isRefreshTokenValid(refreshToken)) {
-                Admin admin = adminRepository.findByRefreshToken(refreshToken);
-                String newAccessToken = this.tokenProvider.generateToken(admin);
-                this.cookieService.addAccessTokenToCookie(newAccessToken, response);
-            } else {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter().write("Invalid or expired refresh token");
+            if (refreshToken == null) {
+                chain.doFilter(request, response);
                 return;
             }
+            return;
         }
 
-        // 다음 필터에 요청 전달
-        chain.doFilter(request, response);
+        TokenStatus tokenStatus = jwtUtil.validateToken(accessToken);
+
+        switch (tokenStatus) {
+            case VALID:
+                Authentication authentication = jwtUtil.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                chain.doFilter(request, response);
+                break;
+            case EXPIRED:
+                    if (jwtUtil.isRefreshTokenValid(refreshToken)) {
+                        Admin admin = adminRepository.findByRefreshToken(refreshToken);
+                        String newAccessToken = this.tokenProvider.generateToken(admin);
+                        this.cookieService.addAccessTokenToCookie(newAccessToken, response);
+                    } else {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        response.getWriter().write("토큰이 만료되었습니다.");
+                        return;
+                    }
+                break;
+            case MALFORMED:
+                System.out.println("잘못된 형식의 토큰입니다.");
+                break;
+            case INVALID:
+                System.out.println("토큰이 비어있거나 올바르지 않습니다.");
+                break;
+        }
     }
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        String method = request.getMethod();
+
+        return !((method.equals("POST") && path.startsWith("/admin/")) ||
+                (method.equals("GET") && path.startsWith("/admin/")) ||
+                (method.equals("DELETE") && path.startsWith("/admin/")));
+    }
 }
