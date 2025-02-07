2338b3376ad9f5fbab8ceef03843dfa79147763b
100gammmit <100minha@naver.com>
2025-02-07 09:51:48 +0900

    Refactor: 프론트엔드 홈페이지 URL 설정파일에서 끌어옴 (#98)

--- backend/src/main/java/com/example/backend/global/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/global/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.example.backend.global.config;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,13 +15,19 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+	private final String clientBaseUrl;
+
+	public CorsConfig(@Value("${CLIENT_BASE_URL}") String clientBaseUrl) {
+		this.clientBaseUrl = clientBaseUrl;
+	}
+
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
 		CorsConfiguration corsConfig = new CorsConfiguration();
 		corsConfig.setAllowCredentials(true);
-		corsConfig.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:3000"));
+		corsConfig.addAllowedOrigin(clientBaseUrl);
 		corsConfig.addAllowedHeader("*");
 		corsConfig.addExposedHeader("Authorization");
 		corsConfig.addAllowedMethod("*");
