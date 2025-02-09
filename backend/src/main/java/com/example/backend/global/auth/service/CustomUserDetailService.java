53cc4043b154ff4b2a2ccf111cd41ca1470248a9
100gammmit <100minha@naver.com>
2025-02-09 17:58:39 +0900

    Feat: 토큰에 내장된 정보로만 CustomUserDetails 객체를 만드는 메서드 추가 (#108)

--- backend/src/main/java/com/example/backend/global/auth/service/CustomUserDetailService.java
+++ backend/src/main/java/com/example/backend/global/auth/service/CustomUserDetailService.java
@@ -4,9 +4,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.backend.domain.member.dto.MemberInfoDto;
 import com.example.backend.domain.member.entity.Member;
 import com.example.backend.domain.member.service.MemberService;
 import com.example.backend.global.auth.model.CustomUserDetails;
+import com.example.backend.global.auth.util.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,11 +22,17 @@ import lombok.RequiredArgsConstructor;
 public class CustomUserDetailService implements UserDetailsService {
 
 	private final MemberService memberService;
+	private final JwtUtil jwtUtil;
 
 	@Override
 	public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		Member member = memberService.findById(Long.valueOf(username));
-		return new CustomUserDetails(member);
+		return new CustomUserDetails(MemberInfoDto.of(member));
+	}
+
+	public CustomUserDetails loadUserByAccessToken(String accessToken) {
+
+		return new CustomUserDetails(jwtUtil.getMemberInfoDto(accessToken));
 	}
 }
