5d2691b45327cd3eb127f0cfd61f7b14300d0b63
100gammmit <100minha@naver.com>
2025-02-12 23:34:58 +0900

    Refactor: 로그인되지 않은 상태에서 모임 생성, 내정보 버튼 navbar에서 비가시처리

--- frontend/src/app/api/auth.ts
+++ frontend/src/app/api/auth.ts
@@ -1,14 +1,14 @@
-import { api } from "./axiosInstance";
+import {api} from "./axiosInstance";
 
 
 // 현재 로그인된 유저 정보 조회
 export const getCurrentUser = async () => {
-    try{
+    try {
         const response = await api.get("/members");
         return response.data;
     } catch (error) {
-        console.error(error);
-        throw error;
+        //console.error(error);
+        return null;
     }
 };
 
--- frontend/src/app/components/MainMenu.tsx
+++ frontend/src/app/components/MainMenu.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { kakaoLogin, kakaoLogout } from "@/app/api";
-import { useState, useEffect} from "react";
-import { getCurrentUser} from "@/app/api";
-
-
+import {getCurrentUser, kakaoLogin, kakaoLogout} from "@/app/api";
+import {useEffect, useState} from "react";
 
 const MainMenu = () => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
@@ -14,15 +11,15 @@ const MainMenu = () => {
         const checkLoginStatus = async () => {
             try {
                 const user = await getCurrentUser();
-                if (user){
+                if (user) {
                     setIsLoggedIn(true);
                 }
-            }catch (error){
+            } catch (error) {
                 setIsLoggedIn(false);
             }
         };
 
-        checkLoginStatus()
+        checkLoginStatus();
     }, []);
 
     const handleKakaoLogin = async () => {
@@ -33,36 +30,38 @@ const MainMenu = () => {
     const handleKakaoLogout = async () => {
         kakaoLogout();
         setIsLoggedIn(false);
-    }
-  return (
-    <nav className="flex justify-between items-center p-4 bg-gray-100 shadow-md">
-      <Link href="/">
-      <h1 className="text-lg font-bold">MOYODANG</h1>
-      </Link>
-      <div className="space-x-4">
-        <Link href="/groups/create">
-        <button className="bg-green-500 text-white px-4 py-2 rounded">
-          모임 만들기
-        </button>
-        </Link>
-        <Link href="/profile" className="text-gray-700">
-          내정보
-        </Link>
-        <Link href="/signup" className="text-gray-700">
-          회원가입
-        </Link>
-          {isLoggedIn ? (
-              <button onClick={handleKakaoLogout} className="text-gray-700">
-                  로그아웃
-              </button>
-          ) : (
-              <button onClick={handleKakaoLogin} className="text-gray-700">
-                  로그인
-              </button>
-          )}
-      </div>
-    </nav>
-  );
+    };
+
+    return (
+        <nav className="flex justify-between items-center p-4 bg-gray-100 shadow-md">
+            <Link href="/">
+                <h1 className="text-lg font-bold">MOYODANG</h1>
+            </Link>
+            <div className="space-x-4">
+                {isLoggedIn && (
+                    <Link href="/groups/create">
+                        <button className="bg-green-500 text-white px-4 py-2 rounded">
+                            모임 만들기
+                        </button>
+                    </Link>
+                )}
+                {isLoggedIn && (
+                    <Link href="/profile" className="text-gray-700">
+                        내정보
+                    </Link>
+                )}
+                {isLoggedIn ? (
+                    <button onClick={handleKakaoLogout} className="text-gray-700">
+                        로그아웃
+                    </button>
+                ) : (
+                    <button onClick={handleKakaoLogin} className="text-gray-700">
+                        로그인
+                    </button>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default MainMenu;
