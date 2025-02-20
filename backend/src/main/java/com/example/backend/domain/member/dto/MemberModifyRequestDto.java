bc6e12fc95827437b749a85535eb6e9993cd0317
100gammmit <100minha@naver.com>
2025-02-20 11:17:41 +0900

    Refactor: 불필요한 Validation옵션 삭제 (#144)

--- backend/src/main/java/com/example/backend/domain/member/dto/MemberModifyRequestDto.java
+++ backend/src/main/java/com/example/backend/domain/member/dto/MemberModifyRequestDto.java
@@ -2,7 +2,6 @@ package com.example.backend.domain.member.dto;
 
 import org.hibernate.validator.constraints.Length;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 /**
@@ -12,7 +11,6 @@ import lombok.Builder;
  */
 @Builder
 public record MemberModifyRequestDto(
-	@NotBlank(message = "닉네임은 공백일 수 없습니다.")
 	@Length(min = 2, max = 10, message = "닉네임은 최소 2글자 이상, 10글자 이하이어야 합니다.")
 	String nickname
 ) {
